b884a016414e3716ab603ff1b053301a7b0a2ff9
Harsh Gupta <133102116+hg9336099029@users.noreply.github.com>
2025-08-06 06:16:11 +0530

    Update authMiddleware.js

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -1,69 +1,30 @@
-import asyncHandler from "express-async-handler";
+
+import User from "../models/user.model.js";
 import jwt from "jsonwebtoken";
-import User from "../models/auth/UserModel.js";
 
-export const protect = asyncHandler(async (req, res, next) => {
-  try {
-    // check if user is logged in
-    const token = req.cookies.token;
+export const protectRoute = async (req, res, next) => {
+	try {
+		const token = req.cookies.jwt;
+		if (!token) {
+			return res.status(401).json({ error: "Unauthorized: No Token Provided" });
+		}
 
-    if (!token) {
-      // 401 Unauthorized
-      return res.status(401).json({ message: "Not authorized, please login!" });
-    }
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    // verify the token
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+		if (!decoded) {
+			return res.status(401).json({ error: "Unauthorized: Invalid Token" });
+		}
 
-    // get user details from the token ----> exclude password
-    const user = await User.findById(decoded.id).select("-password");
+		const user = await User.findById(decoded.userId).select("-password");
 
-    // check if user exists
-    if (!user) {
-      return res.status(404).json({ message: "User not found!" });
-    }
+		if (!user) {
+			return res.status(404).json({ error: "User not found" });
+		}
 
-    // set user details in the request object
-    req.user = user;
-
-    next();
-  } catch (error) {
-    // 401 Unauthorized
-    return res.status(401).json({ message: "Not authorized, token failed!" });
-  }
-});
-
-// admin middleware
-export const adminMiddleware = asyncHandler(async (req, res, next) => {
-  if (req.user && req.user.role === "admin") {
-    // if user is admin, move to the next middleware/controller
-    next();
-    return;
-  }
-  // if not admin, send 403 Forbidden --> terminate the request
-  res.status(403).json({ message: "Only admins can do this!" });
-});
-
-export const creatorMiddleware = asyncHandler(async (req, res, next) => {
-  if (
-    (req.user && req.user.role === "creator") ||
-    (req.user && req.user.role === "admin")
-  ) {
-    // if user is creator, move to the next middleware/controller
-    next();
-    return;
-  }
-  // if not creator, send 403 Forbidden --> terminate the request
-  res.status(403).json({ message: "Only creators can do this!" });
-});
-
-// verified middleware
-export const verifiedMiddleware = asyncHandler(async (req, res, next) => {
-  if (req.user && req.user.isVerified) {
-    // if user is verified, move to the next middleware/controller
-    next();
-    return;
-  }
-  // if not verified, send 403 Forbidden --> terminate the request
-  res.status(403).json({ message: "Please verify your email address!" });
-});
+		req.user = user;
+		next();
+	} catch (err) {
+		console.log("Error in protectRoute middleware", err.message);
+		return res.status(500).json({ error: "Internal Server Error" });
+	}
+};
